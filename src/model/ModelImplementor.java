8c7b77db06ae4fa673954dc688f417d26065eb63
Your Name <you@example.com>
2022-04-13 11:53:37 +0200

    ok

--- src/model/ModelImplementor.java
+++ src/model/ModelImplementor.java
@@ -10,16 +10,16 @@ import nutsAndBolts.PieceSquareColor;
  * @author francoise.perrin
  * 
  * Cete classe fabrique et stocke toutes les PieceModel du Model dans une collection 
- * elle est donc responsable de rechercher et mettre à jour les PieceModel (leur position)
- * En réalité, elle délègue à une fabrique le soin de fabriquer les bonnes PieceModel 
- * avec les bonnes coordonnées
+ * elle est donc responsable de rechercher et mettre ï¿½ jour les PieceModel (leur position)
+ * En rï¿½alitï¿½, elle dï¿½lï¿½gue ï¿½ une fabrique le soin de fabriquer les bonnes PieceModel 
+ * avec les bonnes coordonnï¿½es
  * 
- * En revanche, elle n'est pas responsable des algorithme métiers liés au déplacement des pièces
- * (responsabilité de la classe Model)
+ * En revanche, elle n'est pas responsable des algorithme mï¿½tiers liï¿½s au dï¿½placement des piï¿½ces
+ * (responsabilitï¿½ de la classe Model)
  */
 public class ModelImplementor {
 
-	// la collection de pièces en jeu - mélange noires et blanches
+	// la collection de piï¿½ces en jeu - mï¿½lange noires et blanches
 	private Collection<PieceModel> pieces ;	
 
 	public ModelImplementor() {
@@ -81,9 +81,9 @@ public class ModelImplementor {
 	
 	/**
 	 * @param coord
-	 * @return la pièce qui se trouve aux coordonnées indiquées
+	 * @return la piï¿½ce qui se trouve aux coordonnï¿½es indiquï¿½es
 	 */
-	 PieceModel findPiece(Coord coord) {		// TODO : mettre en "private" après test unitaires
+	 PieceModel findPiece(Coord coord) {		// TODO : mettre en "private" aprï¿½s test unitaires
 		 
 		PieceModel findPiece = null;
 
@@ -97,8 +97,8 @@ public class ModelImplementor {
 	/* (non-Javadoc)
 	 * @see java.lang.Object#toString()
 	 * 
-	 * La méthode toStrong() retourne une représentation 
-	 * de la liste de pièces sous forme d'un tableau 2D
+	 * La mï¿½thode toStrong() retourne une reprï¿½sentation 
+	 * de la liste de piï¿½ces sous forme d'un tableau 2D
 	 * 
 	 */
 	public String toString() {
@@ -107,7 +107,7 @@ public class ModelImplementor {
 		String st = "";
 		String[][] damier = new String[ModelConfig.LENGTH][ModelConfig.LENGTH];
 
-//		// création d'un tableau 2D avec les noms des pièces à partir de la liste de pièces
+//		// crï¿½ation d'un tableau 2D avec les noms des piï¿½ces ï¿½ partir de la liste de piï¿½ces
 //		for(PieceModel piece : this.pieces) {
 //
 //			PieceSquareColor color = piece.getPieceColor();
@@ -118,7 +118,7 @@ public class ModelImplementor {
 //			damier[lig][col ] = stColor ;
 //		}
 
-		// Affichage du tableau formatté
+		// Affichage du tableau formattï¿½
 		st = "     a      b      c      d      e      f      g      h      i      j\n";
 		for ( int lig = 9; lig >=0 ; lig--) {
 			st += (lig+1) + "  ";
